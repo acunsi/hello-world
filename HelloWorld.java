305240ff68f34b34239f8827c495b1154520740b
acunsi <albaiesxm@gmail.com>
2023-09-02 17:26:01 -0400

    added semicolon

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+//Author: Alba Cuns
+//Created: Sep 2, 2023
+//What doess it do:
+//Demonstrates a simple multi-line HelloWorld java demo
+
+public class HelloWorld {
+	
+	public static void HelloWorld(String args[]){
+		System.out.print("Hey Java World!\nLooking Forward To Learning New Things!!");
+	}
+}
